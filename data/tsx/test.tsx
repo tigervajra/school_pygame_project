d9fe828dea2ea7295cf0faab7932e247b0a3866a
gabbagaps <gapandfriends@cock.li>
2025-01-25 17:48:19 +0100

    initial commit

---
+++ data/tsx/test.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="test" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../../../../../Art/drawings/tileset_test.png" width="192" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
